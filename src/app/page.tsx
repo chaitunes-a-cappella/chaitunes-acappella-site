df09ba21afcd3e1bacecef44f87578c71a7b9840
ainsleyfreedman01 <ainsleyfreedman01@gmail.com>
2025-09-28 21:57:58 -0400

    update home pic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { Cookie } from "next/font/google";
 import { Montserrat } from "next/font/google";
+import Image from "next/image";
 
 const cookie = Cookie({ subsets: ["latin"], weight: "400" });
 const montserrat = Montserrat({ subsets: ["latin"] });
@@ -27,23 +28,14 @@ export default function Home() {
   }, [router]);
 
   return (
-    <main className="w-full h-[70vh] flex flex-col items-center justify-center overflow-hidden">
-      {/* Background image */}
-      <div
-        className="absolute top-50% left-50% xl:mt-[4%] lg:mt-[6%] md:mt-[8%] sm:mt-[9%] mt-[10%] w-full h-[70vh] bg-center bg-no-repeat bg-contain"
-        style={{ backgroundImage: "url('/homeImage.png')" }}
+    <div className="flex items-center justify-center h-[70vh] w-full px-4 my-auto">
+      <Image
+        src="/chaitunes_bubbles.png"
+        alt="ChaiTunes Bubbles"
+        width={600}
+        height={600}
+        className="max-h-full max-w-full object-contain"
       />
-
-      {/* Text content */}
-      <div className="relative w-[280px] sm:w-[285px] z-10 text-center py-12 xl:mt-[4%] lg:mt-[6%] md:mt-[8%] mt-[10%] ml-[5.5%] sm:ml-[5%] md:ml-[3%] lg:ml-[3%] xl:ml-[2.5%] 2xl:ml-[2%]">
-        <h1 className={`${cookie.className} text-chai-dark-blue text-4xl sm:text-5xl font-bold mb-4`}>ChaiTunes</h1>
-        <h1 className={`${cookie.className} text-chai-dark-blue text-4xl sm:text-5xl font-bold mb-4`}>A Cappella</h1>
-        <hr className="border-t-4 border-dotted border-chai-dark-blue ml-[4%] sm:ml-[5%] md:ml-[3.5%] lg:ml-[3%] xl:ml-[2.5%] 2xl:ml-[1%]" />
-        <br />
-        <p className={`${montserrat.className} text-chai-dark-blue text-lg -mt-1 flex w-[70%] mx-auto ml-[45px] sm:ml-auto sm:mx-auto`}>
-          Emory University’s only culturally Jewish a cappella group
-        </p>
-      </div>
-    </main>
+    </div>
   );
 }
